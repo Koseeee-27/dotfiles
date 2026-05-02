19a0185e901bc438b15f42de19114529a043621c
Koseeee-27 <kosekose2741@gmail.com>
2026-05-02 17:09:17 +0900

    feat: Raycast Script Commands を追加
    dotfiles 配下で Raycast のスクリプトを版管理する起点として
    raycast-scripts/ を新設。Raycast 側で Script Directory に
    このパスを登録すると配下のスクリプトが自動で拾われる。
    
    - wezterm-new-window.sh: WezTerm を新規ウィンドウで開く

---
+++ raycast-scripts/wezterm-new-window.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New WezTerm Window
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🖥️
+# @raycast.packageName Terminal Launcher
+# @raycast.description WezTerm を新規ウィンドウで開く
+
+open -na WezTerm
