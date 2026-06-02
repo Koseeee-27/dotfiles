ea31df32c61dad4f8e86257533ded8fac70938a5
Koseeee-27 <kosekose2741@gmail.com>
2026-06-03 04:55:06 +0900

    config: NeovimにAstro LSP(lang.astro extra)を追加

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.astro" },
     -- import/override with your plugins
     { import = "plugins" },
   },
